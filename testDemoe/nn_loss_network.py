4774b9aa48933d00b71145ca488a1cc34272a570
gebilxs <1799676669@qq.com>
2022-10-16 11:37:37 +0800

    add loss_network

---
+++ testDemoe/nn_loss_network.py
@@ -0,0 +1,57 @@
+import torchvision.datasets
+from torch import nn
+from torch.nn import Sequential, Conv2d, MaxPool2d, Flatten, Linear
+from torch.utils.data import DataLoader
+
+dataset = torchvision.datasets.CIFAR10("dataset",train=False,transform=torchvision.transforms.ToTensor(),download=True)
+
+dataloader = DataLoader(dataset,batch_size=1)
+class xck(nn.Module):
+    def __init__(self):
+        super(xck, self).__init__()
+    # self.conv1 = Conv2d(3, 32, 5, padding=2)
+    # self.maxpool1 = MaxPool2d(2)
+    # self.conv2 = Conv2d(32, 32, 5, padding=2)
+    # self.maxpool2 = MaxPool2d(2)
+    # self.conv3 = Conv2d(32, 64, 5, padding=2)
+    # self.maxpool3 = MaxPool2d(2)
+    # self.flatten = Flatten()
+    # self.linear1 = Linear(1024, 64)
+    # self.linear2 = Linear(64, 10)
+
+        self.model1 = Sequential(
+            Conv2d(3, 32, 5, padding=2),
+            MaxPool2d(2),
+            Conv2d(32, 32, 5, padding=2),
+            MaxPool2d(2),
+            Conv2d(32, 64, 5, padding=2),
+            MaxPool2d(2),
+            Flatten(),
+            Linear(1024, 64),
+            Linear(64, 10)
+    )
+
+
+    def forward(self, x):
+    # x = self.conv1(x)
+    # x = self.maxpool1(x)
+    # x = self.conv2(x)
+    # x = self.maxpool2(x)
+    # x = self.conv3(x)
+    # x = self.maxpool3(x)
+    # x = self.flatten(x)
+    # x = self.linear1(x)
+    # x = self.linear2(x)
+        x = self.model1(x)
+        return x
+loss = nn.CrossEntropyLoss()
+xck = xck()
+
+for data in dataloader:
+    imgs,targets = data
+    outputs = xck(imgs)
+    # print(outputs)
+    # print(targets)
+    result_loss = loss(outputs,targets)
+    result_loss.backward()
+    print("ok")
