55901b3806498cdf9c848e4df7d810318dea2bd5
gebilxs <1799676669@qq.com>
2022-10-20 18:38:06 +0800

    modify logistic regression

--- dl_demo/logistic regression.py
+++ dl_demo/logistic regression.py
@@ -259,6 +259,25 @@ plt.xlabel('iterations (per hundreds)')
 plt.title("Learning rate =" + str(d["learning_rate"]))
 plt.show()
 
+#different 学习率提供给模型不一样的准确率
+# learning_rates = [0.01, 0.001, 0.0001]
+# models = {}
+# for i in learning_rates:
+#     print ("learning rate is: " + str(i))
+#     models[str(i)] = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 1500, learning_rate = i, print_cost = False)
+#     print ('\n' + "-------------------------------------------------------" + '\n')
+#
+# for i in learning_rates:
+#     plt.plot(np.squeeze(models[str(i)]["costs"]), label= str(models[str(i)]["learning_rate"]))
+#
+# plt.ylabel('cost')
+# plt.xlabel('iterations')
+#
+# legend = plt.legend(loc='upper center', shadow=True)
+# frame = legend.get_frame()
+# frame.set_facecolor('0.90')
+# plt.show()
+
 fname = 'test_demo/2.jpg'
 image = np.array(plt.imread(fname))
 my_image = resize(image,(num_px,num_px)).reshape((1, num_px*num_px*3)).T
